0410d35cf10218042a4c297c61dd18b51813a0e7
Crescent <trtonoy2@gmail.com>
2025-05-19 20:19:06 +0600

    start

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
 import pygame
-print("dfjkha")
+
+pygame.init()
+
+#game window
+screen_width = 1920*0.8
+screen_height = 1080*0.8
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Game")
+
+run= True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    screen.fill((0, 0, 0))  # Fill the screen with black
+    pygame.display.flip()  # Update the display
